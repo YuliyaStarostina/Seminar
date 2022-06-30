21c09481e5f0fe1b8796d46449d847aefe2617c8
Starostina <pankovays@mail.ru>
2022-06-30 16:51:58 +0700

    Сделала задачу 4 к ДЗ семинара 1

---
+++ HomeWork_Zadacha4/Program.cs
@@ -0,0 +1,33 @@
+﻿//Напишите программу, которая принимает на вход три числа и выдает максимальное из этих чисел
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int elementsCount = 3;
+
+        int[] Array = new int[elementsCount];
+
+        for (int i = 0; i < Array.Length; i++)
+        {
+            Console.Write($"Введите число №{i}: ");
+
+            Array[i] = int.Parse(Console.ReadLine()!);
+        }
+
+    int j = 0;
+    int max = 0;
+
+    while (j < elementsCount)
+    {
+        if (max < Array[j])
+        {
+            max = Array[j];
+        }
+        else {}
+    j ++;
+    }
+    Console.WriteLine ($"Максимальное число: " + max);
+
+    }
+}
